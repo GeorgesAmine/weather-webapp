4dc00295ec343d2867d92640893614b5caaf2df8
GeorgesAmine <georgesamine94@gmail.com>
2022-08-14 11:30:48 +0300

    Added gitignore to hide API key

--- weather_app.py
+++ weather_app.py
@@ -1,15 +1,16 @@
 import requests
+import config
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-API_KEY = '1a8c1322a9e9189efa1de0edf989173d'
+
 
 @app.route("/", methods=['GET','POST'])
 def home():
     if request.method == 'POST':
         city = request.form.to_dict()['city']
-        api_url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={API_KEY}'
+        api_url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={config.API_KEY}'
         response = requests.get(url=api_url, params={'units':'metric'}).json()
         return render_template('index.html',response=response)
     return render_template('index.html')
